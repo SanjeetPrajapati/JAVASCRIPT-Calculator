786cc7101b798da88a5faaf15f43469e2c77b845
Sanjeet <prajapatisanjeet50@gmail.com>
2022-12-04 17:51:44 +0530

    js calculator

---
+++ logic.js
@@ -0,0 +1,25 @@
+var screen = document.getElementById("screen");
+
+function clr() {
+    screen.innerHTML = " ";
+    document.getElementById("scr-down").innerHTML = " ";
+    console.log("All cleared")
+}
+
+function btn(val) {
+    screen.innerHTML = screen.innerHTML + val;
+    console.log(val)
+    document.getElementById("scr-down").innerHTML = ' = ' + eval(screen.innerHTML)
+
+}
+
+function del() {
+    screen.innerHTML = screen.innerHTML.slice(0, -1)
+    console.log("1 element deleted from last")
+    document.getElementById("scr-down").innerHTML = ' = ' + eval(screen.innerHTML)
+    document.getElementById("scr-down").innerHTML = " ";
+}
+
+function equal() {
+    document.getElementById("scr-down").innerHTML = ' = ' + eval(screen.innerHTML)
+}
